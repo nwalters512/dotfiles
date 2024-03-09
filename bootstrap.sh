6a03ad1145de677a73400495bda4c10de779502e
Nathan Walters <nwalters512@gmail.com>
2024-03-08 16:36:18 -0800

    Add git aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,3 +46,11 @@ for file in $FILES; do
   fi
 done
 
+###############
+# Git alaises #
+###############
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
