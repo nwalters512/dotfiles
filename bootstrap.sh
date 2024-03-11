bfc6adb4c11d64d6e076790cd1dd5dd071bd5ef3
Nathan Sarang-Walters <nwalters512@gmail.com>
2024-03-11 10:50:57 -0700

    Add more Git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,11 +58,13 @@ for file in $FILES; do
   ln -s $DIR/$file ~/$file
 done
 
-###############
-# Git alaises #
-###############
+##########################
+# Git config and alaises #
+##########################
 
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+git config --global --add --bool push.autoSetupRemote true
